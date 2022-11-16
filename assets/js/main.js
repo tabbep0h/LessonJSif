7f3428916c26d34a0f8125bd5c551dc2c2fe93cf
tabbee <104757339+tabbep0h@users.noreply.github.com>
2022-11-16 21:44:30 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+//--Циклы while и for--//
+
+/*let num = 0;          //--Task 1 
+while (num <= 100) {
+    console.log(num)
+    num++
+}*/
+
+/*let num = 11;         //--Task 2
+while (num <= 33) {
+    console.log(num)
+    num++
+}*/
+
+                        //--Task 3
+/*for (let num = 2;num <=100;num++) { 
+    if (num % 2 == 0){
+        console.log(num);
+    }
+}*/
+
+                        //--Task 4
+/*let result = 0;
+for (let num = 1; num<=100; num++) {
+    result += num
+    console.log(result)
+}*/
+
+//--Работа с for для массивов--//
+
+// let arr = [1, 2, 3, 4, 5];    //--Task 1,2
+// result = 0 
+// for (i in arr)
+//     console.log(arr[i])
+//     result = arr[i] + arr[i++]
+//     console.log(result)
+
+//--Работа с for-in--//
+
+// let obj = {"green":"зеленый","red":"красный","blue":"голубой"} //--Task1
+// for (var key in obj) {
+//     if (obj.hasOwnProperty(key)) {
+//         console.log(key,":",obj['green']); 
+//     }
+// }
+
+// let obj = {"Коля":"200","Вася":"300","Петя":"400"}           //--Task2
+// for (var key in obj) {
+//     if (obj.hasOwnProperty(key)) {
+//         console.log(key + " - зарплата " + obj[key] + " долларов"); 
+//     }
+// }
+
+
+
+
+
+
+
+    
+    
+
+
